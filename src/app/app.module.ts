3acb9fb994dbb3d3fa885ac246b7c0d02916255f
aymen <aymen.sellaouti@gmail.com>
2018-01-17 11:58:57 +0100

    Add binding omponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp1/comp2.component';
+import { BindingComponent } from './binding/binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Comp1Component,
-    Comp2Component
+    Comp2Component,
+    BindingComponent
   ],
   imports: [
     BrowserModule,
