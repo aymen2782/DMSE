b23fbb5c87c778c4d80c801165cb6999741a9fc4
aymen <aymen.sellaouti@gmail.com>
2018-01-17 11:18:47 +0100

    Initialiser projet TP1 DMSE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp1/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
